6b7affc0e9938372225a7b7b95e8b91f9e59a947
Mazhar <mazharuddin1305@gmail.com>
2025-09-14 20:17:41 +0530

    added homepage code only

--- backend/index.js
+++ backend/index.js
@@ -32,13 +32,7 @@ app.use('/api/v1/superAdmin', superAdminRouter);
 app.use('/api/v1/subAdmin', subAdmin);
 app.use('/api/v1/users', userRouter);
 app.use("/api/v1/subAdmin", createNewsRoutes);
-<<<<<<< HEAD
-app.get('/', (req ,res)=>{
-  res.json({
-    messege:"working"
-  })
-})
-=======
+
 
 app.get('/healthcheck',(req,res)=>{
   res.json({
@@ -46,7 +40,7 @@ app.get('/healthcheck',(req,res)=>{
   })
 })
 
->>>>>>> 97923add420845daa1bea419fc287d6a635eeb26
+
 app.listen(process.env.PORT, () => {
   console.log('Server is Running');
 });
