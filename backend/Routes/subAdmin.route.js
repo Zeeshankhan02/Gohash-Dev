1a61ee99f70440c110f3f81fa1f77a0aeaa71242
Zeeshan Khan <142289960+Zeeshankhan02@users.noreply.github.com>
2025-09-18 22:28:46 +0530

    Revert "added social media automation"

--- backend/Routes/subAdmin.route.js
+++ backend/Routes/subAdmin.route.js
@@ -1,13 +1,11 @@
 import { Router } from 'express';
-import { upload } from '../Middlewares/multer.js';
-import { deleteNews, loginPost, uploadMedia, viewNewsCreated } from '../Controllers/subAdmin.controller.js';
+import { deleteNews, loginPost, viewNewsCreated } from '../Controllers/subAdmin.controller.js';
 import { verifyToken } from '../Middlewares/createNewsAuth.js';
 
 
 const router = Router();
 
 router.post("/login",loginPost)
-router.post("/upload",verifyToken,upload.single("media") ,uploadMedia)
 router.get("/viewNewsArticles",verifyToken,viewNewsCreated)
 router.delete("/deleteNews/:articleId",verifyToken,deleteNews)
 
