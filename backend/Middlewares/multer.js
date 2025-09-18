0670170184949165e7dcd789527da908445e8869
Zeeshan Khan <142289960+Zeeshankhan02@users.noreply.github.com>
2025-09-18 22:25:11 +0530

    Revert "fixed the vercel error"

--- backend/Middlewares/multer.js
+++ backend/Middlewares/multer.js
@@ -1,11 +1,9 @@
 import multer from 'multer'
-import { ensureTempDirExists } from '../utils/tempDir.js';
 
 
 const storage = multer.diskStorage({
   destination:function (req,file,cb) {
-    const tempDir = ensureTempDirExists();
-    cb(null, tempDir);
+        cb(null,'./uploads/temp')
   },
   filename: function (req,file,cb) {
     cb(null,file.originalname)
